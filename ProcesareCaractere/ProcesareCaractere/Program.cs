c3789377f624fb7ec711c22b12b108bc22366ae4
vasilefarcas <farcasvasile06@gmail.com>
2022-06-05 12:46:38 +0300

    Add TextFile Class
    Procesare caractere dintr-un fisier text primit ca argument in linia de comanda

---
+++ ProcesareCaractere/ProcesareCaractere/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesareCaractere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TextFile file = new TextFile(@"D:\Drive\OneDrive\Facultate\Semestrul II\Programare orientata pe obiecte\Aplicatii\Teme_OOP\ProcesareCaractere\ProcesareCaractere\FisierSursa.txt");
+            file.viewText();
+            Console.WriteLine("The number of characters including spaces: " + file.CharactersCount);
+            Console.WriteLine("The number of characters without spaces: " + file.CharactersCountWithoutSpaces);
+            Console.WriteLine("The number of vowels: " + file.VowelsCount);
+            Console.WriteLine("The number of consonant: " + file.ConsonantCount);
+            Console.WriteLine("The number of lines: " + file.LinesCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProcesareCaractere/ProcesareCaractere/TextFile.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ProcesareCaractere
+{
+    internal class TextFile
+    {
+        string[] text = new string[10];
+        int numberOfLines = 0;
+        #region Properties
+        public int CharactersCount
+        {
+            get
+            {
+                int numberOfChararcters = 0;
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    numberOfChararcters += text[i].Length;
+                }
+                return numberOfChararcters;
+            }
+        }
+        public int CharactersCountWithoutSpaces
+        {
+            get
+            {
+                int numberOfCharacters = this.CharactersCount;
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    int length = text[i].Length;
+                    for (int j = 0; j < length; j++)
+                    {
+                        if (text[i][j] == ' ')
+                            numberOfCharacters--;
+                    }
+                }
+                return numberOfCharacters;
+            }
+        }
+        public int VowelsCount
+        {
+            get
+            {
+                int vowelsCount = 0;
+                string vowels = "aeiouAEIOU";
+                for (int i = 0; i < numberOfLines; i++)
+                {
+                    int length = text[i].Length;
+                    for (int j = 0; j < length; j++)
+                    {
+                        if (vowels.IndexOf(text[i][j]) != -1)
+                            vowelsCount++;
+                    }
+                }
+                return vowelsCount;
+            }
+        }
+        public int ConsonantCount
+        {
+            get => CharactersCountWithoutSpaces - VowelsCount;
+        }
+        public int LinesCount { get => numberOfLines; }
+        #endregion
+        #region Constructor
+        public TextFile(string path)
+        {
+            TextReader load = new StreamReader(path);
+            string linie;
+            while ((linie = load.ReadLine()) != null)
+            {
+                text[numberOfLines] = linie;
+                numberOfLines++;
+            }
+        }
+        #endregion
+        #region Methods
+        public void viewText()
+        {
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+        #endregion
+    }
+}
